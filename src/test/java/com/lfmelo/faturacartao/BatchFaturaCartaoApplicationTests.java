6822d6572ca5a60f0e6c6f839530344d96388cbe
femelo22 <luiz123jfmg@gmail.com>
2023-08-06 12:52:16 -0300

    feat: config start project

---
+++ src/test/java/com/lfmelo/faturacartao/BatchFaturaCartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfmelo.faturacartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchFaturaCartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
