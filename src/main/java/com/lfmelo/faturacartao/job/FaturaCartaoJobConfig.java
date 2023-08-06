e690a71ef27bc0e2b00d71c60c0585d2d7236097
femelo22 <luiz123jfmg@gmail.com>
2023-08-06 13:47:47 -0300

    feat: created job and step

---
+++ src/main/java/com/lfmelo/faturacartao/job/FaturaCartaoJobConfig.java
@@ -0,0 +1,27 @@
+package com.lfmelo.faturacartao.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class FaturaCartaoJobConfig {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Bean
+	public Job faturaCartaoJob(Step faturaCartaoStep) {
+		return jobBuilderFactory
+				.get("faturaCartaoJob")
+				.start(faturaCartaoStep)
+				.incrementer(new RunIdIncrementer())
+				.build();
+	}
+}
---
+++ src/main/java/com/lfmelo/faturacartao/step/FaturaCartaoStepConfig.java
@@ -0,0 +1,41 @@
+package com.lfmelo.faturacartao.step;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.lfmelo.faturacartao.domain.FaturaCartao;
+
+@Configuration
+public class FaturaCartaoStepConfig {
+	
+	/**
+	 * 
+	 * 	ItemProcessor<I,O>::
+	 *  <I> type of input item
+	 * 	<O> type of output item
+	 */
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step faturaCartaoStep(
+			ItemReader<FaturaCartao> faturaCartaoReader,
+			ItemProcessor<FaturaCartao, FaturaCartao> carregaDadosClienteProcessor,
+			ItemWriter<FaturaCartao> escreveFaturaCartao) {
+		return stepBuilderFactory
+				.get("faturaCartaoStep")
+				.<FaturaCartao, FaturaCartao>chunk(1)
+				.reader(faturaCartaoReader)
+				.processor(carregaDadosClienteProcessor)
+				.writer(escreveFaturaCartao)
+				.build();
+	}
+
+}
